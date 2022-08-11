7d0cf3c1ff50b37f8c495ef381aa0a1cf14eaec3
bdfpplinsinga <daniel@burovoordeboeg.nl>
2022-08-11 06:53:54 +0200

    WIP

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -3,3 +3,27 @@ import Alpine from 'alpinejs'
 window.Alpine = Alpine
 
 Alpine.start()
+
+// Constants
+const toggles = document.querySelectorAll('.js-toggle');
+const menu = document.querySelector('.js-menu');
+const toggle = document.querySelector('.js-toggle-nav');
+const overlay = document.querySelector('.overlay');
+
+// Toggle navigation submenu
+toggles.forEach(toggle => {
+    toggle.querySelector('ul').classList.add('hidden');
+    toggle.querySelector('svg').classList.add('rotate-0');
+    toggle.addEventListener('click', function () {
+        this.querySelector('ul').classList.toggle('hidden');
+        this.querySelector('svg').classList.toggle('rotate-180');
+    })
+});
+
+// Toggle mobile navigation
+toggle.addEventListener('click', function () {
+    overlay.classList.toggle('has-overlay');
+    menu.classList.toggle('hidden');
+});
+
+
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,16 @@
 module.exports = {
-  content: ['./src/**/*.njk', './src/**/*.md',],
+  content: ['./src/**/*.njk', './src/**/*.md', './assets/js/**/*.js',],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        'white': 'hsl(0, 0%, 98%)',
+        'medium-gray': 'hsl(0, 0%, 41%)',
+        'dark-gray': 'hsl(0, 0%, 8%)'
+      },
+      fontFamily: {
+        'sans': ['Epilogue'],
+      }
+    },
   },
   plugins: [],
 }
