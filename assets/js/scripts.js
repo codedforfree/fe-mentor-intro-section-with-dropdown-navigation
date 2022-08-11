36238db4ebb47dc9293616af57bf20d77a835af6
bdfpplinsinga <daniel@burovoordeboeg.nl>
2022-08-11 13:33:55 +0200

    Created nav and mobile nav

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -1,29 +1,46 @@
-import Alpine from 'alpinejs'
-
-window.Alpine = Alpine
-
-Alpine.start()
-
 // Constants
 const toggles = document.querySelectorAll('.js-toggle');
-const menu = document.querySelector('.js-menu');
-const toggle = document.querySelector('.js-toggle-nav');
-const overlay = document.querySelector('.overlay');
+const submenus = document.querySelectorAll('.js-toggle ul.menu');
 
-// Toggle navigation submenu
+// Toggle Dropdowns
 toggles.forEach(toggle => {
-    toggle.querySelector('ul').classList.add('hidden');
-    toggle.querySelector('svg').classList.add('rotate-0');
-    toggle.addEventListener('click', function () {
-        this.querySelector('ul').classList.toggle('hidden');
-        this.querySelector('svg').classList.toggle('rotate-180');
-    })
+    toggle.addEventListener('click', function (e) {
+        // If the clicked item already is open then close it and rotate svg back
+        if (toggle.querySelector('ul.menu').classList.contains('grid')) {
+            toggle.querySelector('ul.menu').classList.remove('grid');
+            toggle.querySelector('ul.menu').classList.add('hidden')
+            toggle.querySelector('svg').classList.remove('rotate-180')
+        } else {
+            // Close all submenus forst before opening the cliked one.
+            toggles.forEach(element => {
+                element.querySelector('ul.menu').classList.remove('grid');
+                element.querySelector('ul.menu').classList.add('hidden');
+                element.querySelector('svg').classList.remove('rotate-180');
+            });
+            // Open submenu
+            toggle.querySelector('ul.menu').classList.remove('hidden');
+            toggle.querySelector('ul.menu').classList.add('grid');
+            toggle.querySelector('svg').classList.add('rotate-180')
+        }
+    });
 });
 
-// Toggle mobile navigation
-toggle.addEventListener('click', function () {
-    overlay.classList.toggle('has-overlay');
-    menu.classList.toggle('hidden');
+// Toggle nav on mobile
+const openBtn = document.querySelector('[data-toggle="open"]');
+const closeBtn = document.querySelector('[data-toggle="close"]');
+const mobileNav = document.querySelector('[data-nav]');
+const overlay = document.querySelector('[data-overlay]');
+
+openBtn.addEventListener('click', function () {
+    mobileNav.classList.remove('hidden');
+    openBtn.classList.add('hidden');
+    closeBtn.classList.remove('hidden');
+    closeBtn.classList.add('flex');
+    overlay.classList.remove('hidden');
 });
-
-
+closeBtn.addEventListener('click', function () {
+    mobileNav.classList.add('hidden');
+    openBtn.classList.remove('hidden');
+    closeBtn.classList.add('hidden');
+    overlay.classList.add('hidden');
+});
